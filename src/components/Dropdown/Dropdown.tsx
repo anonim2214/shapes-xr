26de01c16e1e638e05c8d20f556cafa784c7e545
dmitrymoretz <dmitry.eremichev@gmail.com>
2022-10-25 15:31:20 +0300

    init

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -13,7 +13,7 @@ export default function Dropdown({selected, setSelected, tags = []}) {
         <>
             <button id="dropdownHelperButton" data-dropdown-toggle="dropdownHelper" onClick={() => setIsShow(!isShow)}
                     className="flex-shrink-0 relative text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                    type="button">Dropdown checkbox <svg className="ml-2 w-4 h-4" aria-hidden="true" fill="none"
+                    type="button">Tags<svg className="ml-2 w-4 h-4" aria-hidden="true" fill="none"
                                                          stroke="currentColor" viewBox="0 0 24 24"
                                                          xmlns="http://www.w3.org/2000/svg">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path>
