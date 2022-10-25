6dc1dfcd86975af2d90baaaf2370003d593ff45a
dmitrymoretz <dmitry.eremichev@gmail.com>
2022-10-25 15:45:41 +0300

    init

--- src/components/AdminAddElement/AdminAddElement.tsx
+++ src/components/AdminAddElement/AdminAddElement.tsx
@@ -10,6 +10,12 @@ export default function AdminAddElement ({onSubmit, editableElement}) {
     const [image, setImage] = useState('');
     const [tags, setTags] = useState('');
 
+    const [isButtonAvailable, setIsButtonAvailable] = useState(false);
+
+    useEffect(() => {
+        setIsButtonAvailable(!!(name && price && description && image && tags))
+    }, [name, price, description, image, tags]);
+
     const clearFields = () => {
         setName('');
         setPrice('');
@@ -54,10 +60,11 @@ export default function AdminAddElement ({onSubmit, editableElement}) {
                 <input type="text" id={el[0]} value={el[1]} onChange={(e) => el[2](e.target.value)}
                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
             </div>))}
-            <button type="submit" onClick={handleSubmit}
-                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full  px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            {isButtonAvailable ? (<button type="submit" onClick={handleSubmit} disabled={!isButtonAvailable}
+                     className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full  px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 {editableElement ? 'Modify' : 'Add'}
-            </button>
+            </button>): <div className={"text-center"}>Заполните все поля</div>}
+
             {editableElement && (<button type="submit" onClick={handleCancelEdit}
                      className="text-white w-full mt-2 bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Cancel
             </button>)}
